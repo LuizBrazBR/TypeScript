5f1f43076cd0c6e29baa9947df493182dc0cc0d1
Luiz Braz <brazluiz1000@gmail.com>
2026-04-14 02:48:17 -0300

    Adicionado dicas no docuemnto porém já implementando Object.entries

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
 "use strict";
-// 1 - Crie uma interface UserData para o formulário abaixo
-// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
 // 3 - Adicione um evento de keyup ao formulário
+const form = document.querySelector('#form');
+if (form && form instanceof HTMLElement) {
+    form.addEventListener('keyup', handleForm);
+}
 // 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
-// 5 - Salve UserData no localStorage
-// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
-// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
-const UserData = window.localStorage.getItem('UserData');
-const nome = document.querySelector('#nome');
-const email = document.querySelector('#email');
-const cpf = document.querySelector('#cpf');
-if (UserData && UserData !== null) {
-    const json = JSON.parse(UserData);
-    if (isUserData(json)) {
-        window.UserData = json;
-        if (nome instanceof HTMLInputElement && email instanceof HTMLInputElement && cpf instanceof HTMLInputElement) {
-            nome.value = json.nome || '';
-            email.value = json.email || '';
-            cpf.value = json.cpf || '';
-        }
-    }
-}
-function isUserData(value) {
-    return (!!value &&
-        typeof value === 'object' &&
-        'nome' in value ||
-        'email' in value ||
-        'cpf' in value);
-}
 function handleForm(e) {
     const { target } = e;
     if (target instanceof HTMLInputElement) {
@@ -38,9 +15,28 @@ function handleForm(e) {
             [id]: value
         };
     }
+    // 5 - Salve UserData no localStorage
     localStorage.setItem('UserData', JSON.stringify(window.UserData));
 }
-const form = document.querySelector('#form');
-if (form && form instanceof HTMLElement) {
-    form.addEventListener('keyup', handleForm);
+// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
+function isUserData(value) {
+    return (!!value &&
+        typeof value === 'object' &&
+        'nome' in value ||
+        'email' in value ||
+        'cpf' in value);
+}
+// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
+const UserData = window.localStorage.getItem('UserData');
+if (UserData && UserData !== null) {
+    const json = JSON.parse(UserData);
+    if (isUserData(json)) {
+        window.UserData = json;
+        Object.entries(json).forEach(([key, value]) => {
+            const input = document.querySelector(`#${key}`);
+            if (input instanceof HTMLInputElement) {
+                input.value = value;
+            }
+        });
+    }
 }
--- script.ts
+++ script.ts
@@ -1,47 +1,39 @@
+
+
 // 1 - Crie uma interface UserData para o formulário abaixo
-// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
-// 3 - Adicione um evento de keyup ao formulário
-// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
-// 5 - Salve UserData no localStorage
-// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
-// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
-
- interface Window {
-  UserData: {}
-}
-
 interface UserData {
   nome: string;
   email: string;
   cpf: string;
 }
 
-const UserData = window.localStorage.getItem('UserData')
-const nome = document.querySelector('#nome')
-const email = document.querySelector('#email')
-const cpf = document.querySelector('#cpf')
-
-
-
-if(UserData && UserData !== null ) {
-  const json = JSON.parse(UserData)
-  if (isUserData(json) ) {
-
-    window.UserData = json
-
-    if(nome instanceof HTMLInputElement && email instanceof HTMLInputElement && cpf  instanceof HTMLInputElement) {
-
-      nome.value = json.nome || ''
-      email.value = json.email || ''
-      cpf.value = json.cpf || ''
-
-    }
-
-
-  }
-  
+// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
+ interface Window {
+  UserData: {}
 }
 
+// 3 - Adicione um evento de keyup ao formulário
+const form = document.querySelector('#form')
+if(form && form instanceof HTMLElement) {
+  form.addEventListener('keyup', handleForm)
+}
+
+// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
+function handleForm(e: KeyboardEvent) {
+ const {target } = e
+  if(target instanceof HTMLInputElement) {
+      const value = target.value
+      const id = target.id
+        window.UserData = {
+              ...window.UserData,
+              [id]: value
+          }
+  }
+// 5 - Salve UserData no localStorage
+  localStorage.setItem('UserData', JSON.stringify(window.UserData) )
+}
+
+// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
 function isUserData (value:object): value is UserData {
 return (
 !!value &&
@@ -52,26 +44,19 @@ typeof value === 'object' &&
   );
 }
 
-
-function handleForm(e: KeyboardEvent) {
- const {target } = e
-
-  if(target instanceof HTMLInputElement) {
-  
-      const value = target.value
-      const id = target.id
-      
-        window.UserData = {
-              ...window.UserData,
-              [id]: value
-          }
+// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
+const UserData = window.localStorage.getItem('UserData')
+if(UserData && UserData !== null ) {
+  const json = JSON.parse(UserData)
+  if (isUserData(json) ) {
+    window.UserData = json
+    Object.entries(json).forEach( ([key, value]) => {
+      const input = document.querySelector(`#${key}`)
+      if (input instanceof HTMLInputElement) {
+        input.value = value
+      }
+    })
   }
-
- 
-
-    localStorage.setItem('UserData', JSON.stringify(window.UserData) )
- 
-
 }
 
 
@@ -81,12 +66,7 @@ function handleForm(e: KeyboardEvent) {
 
 
 
-const form = document.querySelector('#form')
 
 
 
-if(form && form instanceof HTMLElement) {
-
-  form.addEventListener('keyup', handleForm)
-}
 
